4ef79c587adbf855895e382589972feb741ca241
Drink Sirichai <drink@finnomena.com>
2020-11-30 00:53:28 +0700

    [ add ] collecton view, delegate, datasource

---
+++ MultiWindowForIPadTutorial/CollectionViewCell/UserCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  UserCollectionViewCell.swift
+//  MultiWindowForIPadTutorial
+//
+//  Created by Drink Sirichai on 30/11/2563 BE.
+//  Copyright © 2563 Drink Sirichai. All rights reserved.
+//
+
+import UIKit
+
+class UserCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- MultiWindowForIPadTutorial/ViewController.swift
+++ MultiWindowForIPadTutorial/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        collectionView.register(UINib(nibName: "UserCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: "UserCollectionViewCell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        return CGSize(width: view.bounds.width, height: 60)
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UserCollectionViewCell", for: indexPath) as! UserCollectionViewCell
+
+        cell.nameLabel.text = "index at \(indexPath)"
+
+        return cell
+    }
+}
