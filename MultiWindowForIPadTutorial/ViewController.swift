47e3c99acb6755c4ffb1d342b35cd9e43bd11b6f
Drink Sirichai <drink@finnomena.com>
2020-11-30 00:28:49 +0700

    [ Add ] Set up initial project

---
+++ MultiWindowForIPadTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiWindowForIPadTutorial
+//
+//  Created by Drink Sirichai on 30/11/2563 BE.
+//  Copyright © 2563 Drink Sirichai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
